f35e3a553f9f2eb10ea6b1dbcb0557aa58cff18d
VicvekSr <Olamideoso65@gmail.com>
2022-06-21 22:07:25 +0000

    intro to js

---
+++ Introduction_to_Javascript/main.js
@@ -0,0 +1,7 @@
+let my_name = "Olamide Oso"
+let my_height = 184
+let my_country = "Nigeria"
+
+console.log("Name: " + my_name)
+console.log("Height: " + my_height +"cm")
+console.log("Country: " + my_country)
